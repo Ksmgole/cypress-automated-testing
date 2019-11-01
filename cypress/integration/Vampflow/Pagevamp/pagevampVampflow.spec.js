8e6e6cbef5b671ad9185b1bf757ae837b1a11d8f
Ksmgole <kusumgole67@gmail.com>
2019-11-01 11:53:30 +0545

    latest updates

--- cypress/integration/Vampflow/Pagevamp/pagevampVampflow.spec.js
+++ cypress/integration/Vampflow/Pagevamp/pagevampVampflow.spec.js
@@ -29,24 +29,6 @@ describe('Pagevamp vampflow',() =>{
                     loginSelector: 'a[href="/auth/auth0/google-oauth2"]',
                     postLoginSelector: '.account-panel'
             }
-            return cy.task('FbocialLogin', socialLoginOptions).then(({cookies}) => {
-                cy.clearCookies()
-          
-                const cookie = cookies.filter(cookie => cookie.name === cookieName).pop()
-                if (cookie) {
-                  cy.setCookie(cookie.name, cookie.value, {
-                    domain: cookie.domain,
-                    expiry: cookie.expires,
-                    httpOnly: cookie.httpOnly,
-                    path: cookie.path,
-                    secure: cookie.secure
-                  })
-          
-                  Cypress.Cookies.defaults({
-                    whitelist: cookieName
-                  })
-                }
-              })
         })
     })
 })
