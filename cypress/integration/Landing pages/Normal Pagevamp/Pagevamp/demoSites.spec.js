1bb3b532815ef03725a1bc4aa2a0e7b7d2827c26
Ksmgole <kusumgole67@gmail.com>
2019-12-18 00:55:36 +0545

    test for demo-sites

---
+++ cypress/integration/Landing pages/Normal Pagevamp/Pagevamp/demoSites.spec.js
@@ -0,0 +1,27 @@
+import pagevampHelper from '../pvHelper'
+
+describe('checking three demo sites',()=>{
+    before(function(){
+        cy.visit(pagevampHelper.getPagevampUrl());
+    })
+ 
+    it('checking J&D Expresso site',()=>{
+        cy.get('.pv-section-cnt .pv-block-divide a[data-url="https://jdespresso1.pagevamp.com"]').click()
+        cy.wait(6000)
+        cy.get('#sample-site .modal-body .pv-loading__overlay').should('have.attr','style','display: none;')
+        cy.get('.modal-body .scroll-wrapper iframe').should('have.attr','src','https://jdespresso1.pagevamp.com')
+    })
+    it('Glamour by Linda Radostová',()=>{
+        cy.get('.pv-section-cnt .pv-block-divide a[data-url="https://glamourbylr1.pagevamp.com"]').click()
+        cy.wait(6000)
+        cy.get('#sample-site .modal-body .pv-loading__overlay').should('have.attr','style','display: none;')
+        cy.get('.modal-body .scroll-wrapper iframe').should('have.attr','src','https://glamourbylr1.pagevamp.com')
+    })
+    it('Meet Social Grill',()=>{
+        cy.get('.pv-section-cnt .pv-block-divide a[data-url="https://meetsocialgrill1.pagevamp.com"]').click()
+        cy.wait(6000)
+        cy.get('#sample-site .modal-body .pv-loading__overlay').should('have.attr','style','display: none;')
+        cy.get('.modal-body .scroll-wrapper iframe').should('have.attr','src','https://meetsocialgrill1.pagevamp.com')
+    })
+
+}) 
